b36d1cfa8dbafec2a553cfaeb25b001d80b6798f
jeremy331 <79677029+jeremy331@users.noreply.github.com>
2022-05-13 16:16:13 +0800

    Add files via upload

---
+++ week09_opencv/week09_opencv.cpp
@@ -0,0 +1,9 @@
+#include <opencv/highgui.h>
+int main()
+{ /// Ipl 是 Intel performance library 的縮寫
+
+	IplImage * img = cvLoadImage("test3.jpg"); //讀圖
+	cvShowImage("week09", img); //秀圖
+	cvWaitKey(0); //等任意鍵繼續
+
+}
